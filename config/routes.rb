001869b3cf63f4f477da0032a603416185f1f0e7
NGOC HOA (DAVID) BUI <buingochoa1987@gmail.com>
2018-11-06 17:03:32 +1100

    rails crud task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # to get all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # to ask user for tasks new in form
+  get 'tasks/new', to: 'tasks#new'
+
+  # to create new task
+  post 'tasks', to: 'tasks#create'
+
+    # to get view specific task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # to ask user for tasks edit in form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # to update task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # to delet a specific task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
